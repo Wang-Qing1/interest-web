bd773a2821adad029ababe81318ace9d03ae21db
wangq <wangqing@nianhuatech.com>
2023-05-25 11:09:36 +0800

    引入AntDesign v5

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {ConfigProvider} from "antd";
+import zhCN from 'antd/locale/zh_CN';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App/>
+    <ConfigProvider locale={zhCN}>
+      <App/>
+    </ConfigProvider>
   </BrowserRouter>
 );
