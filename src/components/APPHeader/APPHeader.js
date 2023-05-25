4ff5b2ecb830b756afde5c11992724502eb848bb
wangq <wangqing@nianhuatech.com>
2023-05-25 17:56:59 +0800

    二维码测试

--- src/components/APPHeader/APPHeader.js
+++ src/components/APPHeader/APPHeader.js
@@ -3,7 +3,7 @@
  */
 import './APPHeader.css';
 import React from "react";
-import {message, Tooltip} from "antd";
+import {message, QRCode, Tooltip} from "antd";
 import {GlobalConfig} from "../../config/GlobalConfig";
 import {
   ExclamationCircleOutlined,
@@ -14,6 +14,19 @@ import {
 } from "@ant-design/icons";
 
 export default class APPHeader extends React.Component {
+  /**
+   * 返回二维码
+   */
+  renderQRCode(value, icon) {
+    return (
+      <QRCode
+        value={value || "-"}
+        errorLevel={"H"}
+        icon={icon || undefined}
+      />
+    );
+  }
+
   render() {
     return (
       <div className={'app-header'}>
@@ -25,13 +38,13 @@ export default class APPHeader extends React.Component {
             {/* 新开页面 */}
             <GithubOutlined className={'app-other-icons'} onClick={() => window.open("https://www.baidu.com")}/>
           </Tooltip>
-          <Tooltip title={'WeiBo'}>
+          <Tooltip color={"#FFFFFF"} title={() => this.renderQRCode("WebBo", "https://img0.baidu.com/it/u=3336255710,1936944088&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500")}>
             <WeiboCircleOutlined className={'app-other-icons'} onClick={() => message.info("WeiBo")}/>
           </Tooltip>
-          <Tooltip title={'WeChat'}>
+          <Tooltip color={"#FFFFFF"} title={() => this.renderQRCode("WeChat", "https://img1.baidu.com/it/u=3582852405,4232068154&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500")}>
             <WechatOutlined className={'app-other-icons'} onClick={() => message.info("WeChat")}/>
           </Tooltip>
-          <Tooltip title={'QQ'}>
+          <Tooltip color={"#FFFFFF"} title={() => this.renderQRCode("QQ", "https://img2.baidu.com/it/u=359402080,1075457947&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500")}>
             <QqOutlined className={'app-other-icons'} onClick={() => message.info("QQ")}/>
           </Tooltip>
           <Tooltip title={"平台信息"}>
