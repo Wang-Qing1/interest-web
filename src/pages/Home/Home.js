6ffb725b2b3884f5e56560e60a9b3b8b3917f25c
wangq <wangqing@nianhuatech.com>
2023-06-07 18:04:47 +0800

    首页布局

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,57 @@
  */
 import './Home.css';
 import Base from "../Base/Base";
+import {Card, Col, Row} from "antd";
 
 export default class Home extends Base {
   renderContent() {
     return (
       <div className={'app-home'}>
-        首页
+        <Row gutter={[16,16]} style={{marginBottom: 8}}>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={[16,16]}>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+            <Card title="Card title" bordered={false}>
+              Card content
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
